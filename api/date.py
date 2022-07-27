c65c817dfbceff15e0b0223ef0f51672abcef385
洪健翔 Hung, Chien-Hsiang <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-27 14:24:02 +0800

    get user

--- api/date.py
+++ api/date.py
@@ -4,8 +4,11 @@ from datetime import datetime
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    path = self.path
+    user = path.split('?')[1]
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
+    self.wfile.write(f'hi {user}')
     return
