d39ddf98e4845270aad5b08c44a70e7e7ae12af2
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-27 18:54:24 +0800

    Update basic.py

--- api/basic.py
+++ api/basic.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler
+from crawler import kaggle_crawler
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     path = self.path
     user = path.split('?')[1]
+    userAvatarUrl, displayName, country, city, occupation, organization, performanceTier, performanceTierCategory, userJoinDate, userAchieveUrl = kaggle_crawler(user)
     self.send_response(200)
     self.send_header('Content-type', 'text/html')
     self.end_headers()
@@ -13,38 +15,43 @@ class handler(BaseHTTPRequestHandler):
 <!DOCTYPE html>
 <html style="max-width:540px; max-height:242.78px">
 <head>
+    <meta property="og:url" content="https://chienhsiang-hung.github.io/"/>
+    <meta name="author" content="Hung, Chien-Hsiang"/>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
-    <style>
-
-    </style>
 </head>
 <body>
 
-<div class="card mb-3" style="max-width: 540px;">
+<div class="card mb-3" style="max-width:540px; cursor:pointer;" onclick="nav_to_kaggle()">
   <div class="card-header">
     <img src="https://www.kaggle.com/static/images/site-logo.svg" width="82" >
   </div>
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="https://storage.googleapis.com/kaggle-avatars/images/6294032-kg.png" class="img-fluid rounded-start" alt="...">
+      <img src="'''+userAvatarUrl+'''" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-5">
       <div class="card-body">
-        <h5 class="card-title">'''+user+'''</h5>
-        <p class="card-text">IT Manager, DDI at WAM</p>
-        <p class="card-text">Taipei, Taiwan</p>
-        <p class="card-text"><small class="text-muted">Joined 2 years ago · last seen in the past day</small></p>
+        <h5 class="card-title">'''+displayName+'''</h5>
+        <p class="card-text">'''+occupation+''' at '''+organization+'''</p>
+        <p class="card-text">'''+city+''', '''+country+'''</p>
+        <p class="card-text"><small class="text-muted">Joined at '''+userJoinDate+'''</small></p>
       </div>
     </div>
     <div class="col-md-3" style="text-align: center;">
-        <img src="https://www.kaggle.com/static/images/tiers/expert@192.png" class="img-fluid rounded-start" alt="...">
-        <h6 style="color:#95628f;">Competitions Expert</h6>
+        <img src="'''+userAchieveUrl+'''" class="img-fluid rounded-start" alt="...">
+        <h6 style="color:#95628f;">'''+performanceTierCategory+''' '''+performanceTier+'''</h6>
       </div>
   </div>
 </div>
 
+<script>
+    function nav_to_kaggle() {
+        window.open('https://www.kaggle.com/chienhsianghung', '_blank').focus();
+    }
+</script>
 </body>
 </html>
 
