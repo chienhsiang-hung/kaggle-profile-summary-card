15c9e89d965ed70e7d886df97ffc9bdef4a9bf24
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-27 15:39:12 +0800

    Update date.py

--- api/date.py
+++ api/date.py
@@ -7,7 +7,7 @@ class handler(BaseHTTPRequestHandler):
     path = self.path
     user = path.split('?')[1]
     self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
+    self.send_header('Content-type', 'text/html')
     self.end_headers()
     self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
     self.wfile.write(f'hi {user}'.encode())
