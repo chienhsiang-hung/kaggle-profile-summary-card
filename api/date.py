562c8e6b55d10ba70d3e1835cdba4e0f36cc7892
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-07-27 16:46:03 +0800

    adjust html max size

--- api/date.py
+++ api/date.py
@@ -15,7 +15,7 @@ class handler(BaseHTTPRequestHandler):
     html = '''
 
 <!DOCTYPE html>
-<html>
+<html style="max-width:540px; max-height:242.78px">
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
