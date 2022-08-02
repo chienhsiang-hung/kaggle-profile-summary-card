85773b7720bf3c5767f6bc2f9dfe926d2335cd80
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-08-02 16:51:57 +0800

    Create mongodb_update_test.py

---
+++ api/test/mongodb_update_test.py
@@ -0,0 +1,16 @@
+import pymongo
+import os
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+
+        client = pymongo.MongoClient( os.environ.get('MONGODB_URI') )
+        db = client.test
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+        self.wfile.write(os.environ.get('MONGODB_URI').encode())
+        return
