fc5a4a1bd0cebd9cfbde9830890ce82a42e2dfd0
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-08-02 21:20:21 +0800

    Update svg-allround.py

--- api/svg-allround.py
+++ api/svg-allround.py
@@ -42,8 +42,11 @@ class handler(BaseHTTPRequestHandler):
       '''
 
     else:
-      userAchieveUrl = img_to_datauri(userAchieveUrl)
       userAvatarUrl = img_to_datauri(userAvatarUrl)
+      competitionsAchieveUrl = img_to_datauri(competitionsAchieveUrl)
+      scriptsSummaryAchieveUrl = img_to_datauri(scriptsSummaryAchieveUrl)
+      discussionsSummaryAchieveUrl = img_to_datauri(discussionsSummaryAchieveUrl)
+      datasetsAchieveUrl = img_to_datauri(datasetsAchieveUrl)
 
       html = '''
           <svg width="540" height="250" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -52,15 +55,15 @@ class handler(BaseHTTPRequestHandler):
             <title>Layer 1</title>
             <rect fill="#ffffff" height="239" id="svg_4" rx="10" ry="10" stroke="#666666" stroke-linecap="round" stroke-linejoin="round" transform="matrix(1 0 0 1 0 0)" width="531" x="4" y="5"/>
             <rect fill="#e5e5e5" height="57" id="svg_6" rx="10" ry="10" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" width="529" x="5" y="7"/>
-            <image height="41.46" id="svg_2" width="123" x="13" xlink:href="https://www.kaggle.com/static/images/site-logo.svg" y="15"/>
+            <image height="41.46" id="svg_2" width="123" x="13" xlink:href="'''+util.kaggle_svg_dataurl+'''" y="15"/>
             <line fill="none" id="svg_7" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" transform="matrix(1 0 0 1 0 0)" x1="7" x2="534" y1="65" y2="66"/>
-            <image height="171" id="svg_10" transform="matrix(1 0 0 1 0 0)" width="171" x="6" xlink:href="https://storage.googleapis.com/kaggle-avatars/images/6294032-kg.png" y="71"/>
+            <image height="171" id="svg_10" transform="matrix(1 0 0 1 0 0)" width="171" x="6" xlink:href="data:image/png; base64,'''+userAvatarUrl+'''" y="71"/>
             <text fill="#000000" font-family="Sans-serif" font-size="19" font-weight="bold" id="svg_12" stroke="#666666" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" text-anchor="start" transform="matrix(1 0 0 1 0 0)" x="189.67" xml:space="preserve" y="98">Chien-Hsiang, Hung</text>
             <text fill="#999999" font-family="Sans-serif" font-size="14" id="svg_1" stroke="#666666" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" text-anchor="start" x="392" xml:space="preserve" y="28">Joined at 2022-12-02</text>
-            <image height="46" id="svg_3" width="46" x="210" xlink:href="https://www.kaggle.com/static/images/tiers/expert@192.png" y="122"/>
-            <image height="46" id="svg_8" width="46" x="295" xlink:href="https://www.kaggle.com/static/images/tiers/expert@192.png" y="122"/>
-            <image height="46" id="svg_9" width="46" x="380" xlink:href="https://www.kaggle.com/static/images/tiers/expert@192.png" y="122"/>
-            <image height="46" id="svg_11" width="46" x="465" xlink:href="https://www.kaggle.com/static/images/tiers/expert@192.png" y="122"/>
+            <image height="46" id="svg_3" width="46" x="210" xlink:href="data:image/png; base64,'''+competitionsAchieveUrl+'''" y="122"/>
+            <image height="46" id="svg_8" width="46" x="295" xlink:href="data:image/png; base64,'''+scriptsSummaryAchieveUrl+'''" y="122"/>
+            <image height="46" id="svg_9" width="46" x="380" xlink:href="data:image/png; base64,'''+discussionsSummaryAchieveUrl+'''" y="122"/>
+            <image height="46" id="svg_11" width="46" x="465" xlink:href="data:image/png; base64,'''+datasetsAchieveUrl+'''" y="122"/>
             <text fill="#95628f" font-family="Sans-serif" font-size="12" font-weight="bold" id="svg_5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" text-anchor="middle" transform="matrix(1 0 0 1 0 0)" x="235" xml:space="preserve" y="197">Competitions</text>
             <text fill="#95628f" font-family="Sans-serif" font-size="12" font-weight="bold" id="svg_13" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" text-anchor="middle" transform="matrix(1 0 0 1 0 0)" x="320.84" xml:space="preserve" y="197">Datasets</text>
             <text fill="#95628f" font-family="Sans-serif" font-size="12" font-weight="bold" id="svg_14" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" text-anchor="middle" transform="matrix(1 0 0 1 0 0)" x="399.84" xml:space="preserve" y="197">Notebooks</text>
